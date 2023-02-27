32c4b408bf0e75ef2cba7bb1eb59e0f634758bc8
AndreyZburivskii <32275030+AndreyZburivskii@users.noreply.github.com>
2023-02-27 12:52:27 +0200

    Update memory_usage.sh

--- scipts/memory_usage.sh
+++ scipts/memory_usage.sh
@@ -8,11 +8,10 @@ do
   # iterate over each process and print its details
   for i in $(pgrep slk)
   do
-    echo "$(date +'%Y-%m-%d %H:%M:%S');$(pmap -x $i | grep total | awk '{printf "%.2fMB;%.2fMB;%.2fMB;",$3/1024,$4/1024,$5/1024}')$(ps -p $i -o comm=)"
+    echo "$(date +'%Y-%m-%d %H:%M:%S');$(pmap -x $i | grep total | awk '{printf "%.2f;%.2f;%.2f;",$3/1024,$4/1024,$5/1024}')$(ps -p $i -o comm=)"
   done
 
   # wait for 1 hour before running again
   sleep 3600
 done
-
 
